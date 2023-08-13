ebc6278461cd6822e8aef278a397ec5785ec074e
bwk5140 <brianwkarimi@gmail.com>
2023-08-13 11:09:49 +0300

    Added real-time search results for employee and product classes

--- AeroArchive/AeroArchive/Views/SearchEmployeesPage.xaml.cs
+++ AeroArchive/AeroArchive/Views/SearchEmployeesPage.xaml.cs
@@ -1,21 +1,66 @@
-﻿using AeroArchive.ViewModels;
+﻿using AeroArchive.Models;
+using AeroArchive.ViewModels;
+using System.Diagnostics;
+using System;
 using Xamarin.Forms;
+using System.Collections.ObjectModel;
 
 namespace AeroArchive.Views
 {
     public partial class SearchEmployeesPage : ContentPage
     {
         readonly SearchEmployeesViewModel _viewModel;
+        public ObservableCollection<Employee> Employees { get; }
         public SearchEmployeesPage()
         {
             InitializeComponent();
             BindingContext = _viewModel = new SearchEmployeesViewModel();
+            Employees = _viewModel.Employees;
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
             _viewModel.OnAppearing();
+        }
+
+        private async void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                Employees.Clear();
+                string lowercaseName, lowercaseID, lowercaseRole, lowercaseEmail;
+                var items = await App.Employee_Database.GetEmployeeDetsAsync();
+
+                foreach (var item in items)
+                {
+                    lowercaseName = item.FullName.ToLower();
+                    lowercaseID = item.EmployeeID.ToLower();
+                    lowercaseRole = item.Role.ToLower();
+                    lowercaseEmail = item.Email.ToLower();
+
+                    if (item.FullName.Contains(e.NewTextValue) || lowercaseName.Contains(e.NewTextValue))
+                    {
+                        Employees.Add(item);
+                    }
+                    else if (item.EmployeeID.Contains(e.NewTextValue) || lowercaseID.Contains(e.NewTextValue))
+                    {
+                        Employees.Add(item);
+                    }
+                    else if (item.Role.Contains(e.NewTextValue) || lowercaseRole.Contains(e.NewTextValue))
+                    {
+                        Employees.Add(item);
+                    }
+                    else if (item.Email.Contains(e.NewTextValue) || lowercaseEmail.Contains(e.NewTextValue))
+                    {
+                        Employees.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
         }
     }
 }
--- AeroArchive/AeroArchive/Views/SearchProductsPage.xaml.cs
+++ AeroArchive/AeroArchive/Views/SearchProductsPage.xaml.cs
@@ -1,21 +1,62 @@
 ﻿using AeroArchive.ViewModels;
+using System.Diagnostics;
+using System;
 using Xamarin.Forms;
+using AeroArchive.Models;
+using System.Collections.ObjectModel;
 
 namespace AeroArchive.Views
 {
     public partial class SearchProductsPage : ContentPage
     {
         readonly SearchProductsViewModel _viewModel;
+        public ObservableCollection<Item> Items { get; }
         public SearchProductsPage()
         {
             InitializeComponent();
             BindingContext = _viewModel = new SearchProductsViewModel();
+            Items = _viewModel.Items;
         }
         
         protected override void OnAppearing()
         {
             base.OnAppearing();
             _viewModel.OnAppearing();
+        }
+
+        private async void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                Items.Clear();
+                string lowercaseItem, lowercaseWarranty, lowercaseDescription;
+                var items = await App.Prod_Database.GetProductDetsAsync();
+
+                foreach (var item in items)
+                {
+                    lowercaseItem = item.Text.ToLower();
+                    lowercaseWarranty = item.WarrantyStatus.ToLower();
+                    lowercaseDescription = item.Description.ToLower();
+
+                    if (item.Text.Contains(e.NewTextValue) || lowercaseItem.Contains(e.NewTextValue))
+                    {
+                        Items.Add(item);
+                    }
+                    else if (item.WarrantyStatus.Contains(e.NewTextValue) || lowercaseWarranty.Contains(e.NewTextValue))
+                    {
+                        Items.Add(item);
+                    }
+                    else if (item.Description.Contains(e.NewTextValue) || lowercaseDescription.Contains(e.NewTextValue))
+                    {
+                        Items.Add(item);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
         }
     }
 }
